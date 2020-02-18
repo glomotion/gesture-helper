d530140a5895406498995220fc5ccf54cd2ab5f1
tim <tim@glo.id.au>
2020-02-18 14:07:02 +1100

    adjust demo to test edge case

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 import GestureHelper from "../src/gesture-helper.js";
 
 const demoOutput = document.getElementById("output");
-const gestureCtrl = new GestureHelper(document.body, {
+const gestureCtrl = new GestureHelper(document.getElementById("touchArea"), {
   sensitivity: 0,
   passive: false,
   capture: false,
@@ -16,14 +16,14 @@ gestureCtrl.on("tap", e => {
 });
 
 
-gestureCtrl.on("pan.start", function(ev) {
+gestureCtrl.on("pan.start", function (ev) {
   demoOutput.innerHTML = JSON.stringify(ev);
 });
 
 gestureCtrl.on("pan.all", handleAll);
 // gestureCtrl.on("pan.x.**", handleX);
 
-gestureCtrl.on("pan.end", function(ev) {
+gestureCtrl.on("pan.end", function (ev) {
   demoOutput.innerHTML = JSON.stringify(ev);
   clear();
 });
